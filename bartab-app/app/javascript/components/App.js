c46458394b1a1e326f78f2e01bb0c1c8b54e2b81
Peter <petersarll@gmail.com>
2019-06-17 23:00:23 +0000

    added logging in instructions to the root.html.erb, have not attached the login to the button on the main page yet

--- bartab-app/app/javascript/components/App.js
+++ bartab-app/app/javascript/components/App.js
@@ -10,9 +10,11 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <HomePage />
-        <br /><br /><br />
-        <div id="menu">
-          <Login />
+        <br />
+        <br />
+        <br />
+          <div id="menu">
+          <Login /> <button id="User">Beer Drinker</button>
         </div>
       </React.Fragment>
     );
