49ec771e8c228e2083c22a58a27e1e0bada16737
Ubuntu <ubuntu@ip-172-31-47-27.us-east-2.compute.internal>
2019-06-18 00:06:07 +0000

    fixed error by adding link to log in

--- bartab-app/app/javascript/components/App.js
+++ bartab-app/app/javascript/components/App.js
@@ -11,26 +11,49 @@ import UserHome from './pages/user/UserHome'
 import {Container, Button} from "reactstrap"
 
 class App extends React.Component {
+   constructor(props){
+    super(props)
+      this.state = {
+      }
+  }
+  
   render () {
+    const {
+      admin_logged_in, 
+      admin_sign_in_route, 
+      admin_sign_out_route,
+      user_logged_in,
+      user_sign_in_route,
+      user_sign_out_route,
+    } =this.props
+  
     return (
       <React.Fragment>
-        <HomePage />
-        <br /><br /><br />
-        <div id="menu">
-          <Login /><Login /><Login />
-        </div>
-        
-        <Router>
-          <Button href='/admin_home'>Admin Login
-          </Button>
+        <Container>
+          <HomePage />
+          <br /><br /><br />
+          <div id="menu">
+            <Login /><Login /><Login />
+          </div>
           
-          <Button href='/user_home'>User Login
-          </Button>
-          
-          <Route path="/admin_home" component={AdminHome} />
-          <Route path="/user_home" component={UserHome} />
-        </Router>
-        
+          <Router>
+            
+            <Button href='/admin_home'>Admin Login
+            </Button>
+            
+            <Button href='/user_home'>User Login
+            </Button>
+            
+            <Route path="/admin_home" render={(props) => <AdminHome {...props} 
+              admin_logged_in={admin_logged_in}
+              admin_sign_in_route={admin_sign_in_route}
+              admin_sign_out_route={admin_sign_out_route}
+            />} />
+            
+            <Route path="/user_home" component={UserHome} />
+            
+          </Router>
+        </Container>
       </React.Fragment>
     );
   }
--- bartab-app/app/javascript/components/pages/admin/AdminHome.js
+++ bartab-app/app/javascript/components/pages/admin/AdminHome.js
@@ -9,30 +9,44 @@ import Profile from "./Profile";
 import Menu from "./Menu";
 
 class AdminHome extends React.Component {
+   constructor(props){
+    super(props)
+      this.state = {
+      }
+  }
   render () {
+     const {admin_logged_in, 
+        admin_sign_in_route, 
+        admin_sign_out_route,
+     }=this.props
     return (
       <React.Fragment>
-        <Router>
-        
-          <Nav>
-            <NavItem>
-              <NavLink href="/admin_home/open_tabs">Open Tabs</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/admin_home/menu">Menu</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/admin_home/profile">Profile</NavLink>
-            </NavItem>
-          </Nav>
-          
-          <Route path="/admin_home/open_tabs" exact component={OpenTabs} />
-          <Route path="/admin_home/menu" exact component={Menu} />
-          <Route path="/admin_home/profile" exact component={Profile} />
-          
+          <Router>
+            <div>
+              {admin_logged_in &&
+                <Nav>
+                  <NavItem>
+                    <NavLink href="/admin_home/open_tabs">Open Tabs</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/admin_home/menu">Menu</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/admin_home/profile">Profile</NavLink>
+                  </NavItem>
+                </Nav>
+              }
+            {!admin_logged_in &&
+              <a href={admin_sign_in_route}>Log In</a>
+              
+            }
+
+            <Route path="/admin_home/open_tabs" exact component={OpenTabs} />
+            <Route path="/admin_home/menu" exact component={Menu} />
+            <Route path="/admin_home/profile" exact component={Profile} />
+          </div>
         </Router>
-        
-      </React.Fragment>
+    </React.Fragment>
     );
   }
 }
