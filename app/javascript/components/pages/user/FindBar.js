109d79309498f27b43ad69b09609ce343dea1108
Ubuntu <ubuntu@ip-172-31-16-151.us-east-2.compute.internal>
2019-07-21 22:37:03 +0000

    added procfile

---
+++ app/javascript/components/pages/user/FindBar.js
@@ -0,0 +1,29 @@
+import React from "react"
+import MapContainer from "./MapContainer"
+import StoreList from './StoreList'
+
+class FindBar extends React.Component {
+  render () {
+    const {stores, openTab, success} = this.props
+    return (
+      <div className="findbar" >
+        <div className="mapcomponent">
+          <MapContainer
+            stores={stores}
+            openTab={openTab}
+            success={success}
+          />
+        </div>
+        <StoreList
+          stores={stores}
+          openTab={openTab}
+        />
+        {this.props.success &&
+        	<Redirect to="/user_home/opentabs" />
+        }
+      </div>
+    );
+  }
+}
+
+export default FindBar
