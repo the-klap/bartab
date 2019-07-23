c256760c9c9bce1b6367dc8be96c9bd4de5dd7e1
Ubuntu <ubuntu@ip-172-31-16-151.us-east-2.compute.internal>
2019-07-23 01:11:48 +0000

    working locally

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '*path', to: 'pages#root', constraints: ->(request){ request.format.html? }
-  root to: 'pages#root'
+  get '*path', to: 'pages#index', constraints: ->(request){ request.format.html? }
+  root to: 'pages#index'
 end
